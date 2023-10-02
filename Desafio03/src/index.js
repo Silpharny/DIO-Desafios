afc204dd8bf0ac3ff41f29cb27841c8658c729c2
Silpharny <silpharny@gmail.com>
2023-10-02 12:42:16 -0300

    desafio 03

---
+++ Desafio03/src/index.js
@@ -0,0 +1,37 @@
+class Hero {
+    constructor(name, age, type){
+        this.name = name
+        this.age = age
+        this.type = type
+    }
+    typeHero(type){
+        let attack = ""
+        switch (type) {
+            case "Guerreiro": 
+                attack = "espada"
+                console.log("O " + type + " atacou usando " + attack)
+                break;
+            case "Mago":
+                attack = "magia"
+                console.log("O " + type + " atacou usando " + attack)
+                break;
+            case "Monge":
+                attack = "artes marciais"
+                console.log("O " + type + " atacou usando " + attack)
+                break;
+            case "Ninja":
+                attack = "shuriken"
+                console.log("O " + type + " atacou usando " + attack)
+                break;
+            default: "Escolha um herói válido!"
+                break;
+        }
+    }
+}
+
+let actionHero = new Hero("Mur", 24, "Guerreiro")
+
+actionHero.typeHero("Guerreiro")
+actionHero.typeHero("Mago")
+actionHero.typeHero("Monge")
+actionHero.typeHero("Ninja")
